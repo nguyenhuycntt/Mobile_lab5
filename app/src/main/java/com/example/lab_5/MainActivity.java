40ce56d37e3926797cf2f6f233cdf8765331f583
nguyenhuycntt <muadongthattinh3456@gmail.com>
2021-04-14 20:23:24 +0700

    Lab 5

---
+++ app/src/main/java/com/example/lab_5/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.lab_5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+    private final LinkedList<String> mWordList = new LinkedList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        for (int i = 0; i < 20; i++) {
+            mWordList.addLast("Word " + i);
+    }
+}
